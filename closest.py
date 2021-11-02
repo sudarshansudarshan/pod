c8a550fb768fec28c72b8d4abe9f98937a832409
Sudarshan Iyengar <sudarshaniyengar@Sudarshans-iMac.local>
2021-11-02 20:43:58 +0530

    nothing much

--- closest.py
+++ closest.py
@@ -1,4 +1,8 @@
 def populate(n,i,j):
+    """
+    This will just populate the list L with random numbers between i and j. It
+    will populate n numbers
+    """
     L=[]
     for i in range(n):
         L.append(random.randint(i,j))
