5404228b51ce4aeaa29c5663b125c7b949dd221c
Sudarshan Iyengar <sudarshaniyengar@Sudarshans-iMac.local>
2021-12-14 08:04:53 +0530

    spit function added

--- compress.py
+++ compress.py
@@ -1,5 +1,17 @@
 def distribution():
     d={'A':0.08,'B':0.11,'C':0.23,'D':0.05,'E':0.07,'F':0.18,'G':0.02,'H':0.15,'I':0.01,'J':0.10}
     return d
-d=distribution()
-print(d)
+
+def spit():
+    """This function will respect the distribution function and give you a
+    letter non-uniformly at random. Note that everytime you call this function,
+    it will just give you one letter (capital)."""
+
+
+    
+
+
+    
+
+
+
